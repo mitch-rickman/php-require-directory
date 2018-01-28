14dd6937bb56d57f3bf38864eb3a0ac898ac8533
Mitch Rickman <mitchrickman@gmail.com>
2018-01-28 15:16:18 -0800

    make function instead of class

--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 
 namespace RequireDirectory;
 
-class Autoload {
-    public function __construct( $path ) {
-        if ( file_exists($path) ) {
-            foreach ( glob( $path . '*.php' ) as $filename ) {
-                require_once( $filename );
-            }
+function Autoload ( $path ) {
+    if ( file_exists($path) ) {
+        foreach ( glob( $path . '*.php' ) as $filename ) {
+            require_once( $filename );
         }
-        else {
-            error_log( "Directory does not exist: $path" );
-        }
+    }
+    else {
+        error_log( "Directory does not exist: $path" );
     }
 }
